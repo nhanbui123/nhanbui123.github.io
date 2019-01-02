a33c43077e07e0091bfaf79b31981eb193600ac1
unknown <nhanbui.toannang@gmail.com>
2019-01-03 01:57:29 +0700

    da up xong

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ taodulieu();
  $('ul.anh-jobs').masonry({
 					  // options
 					 
-					  itemSelector: 'li.anh',
+					  itemSelector: 'li.anh ',
 					  filtersGroupSelector: 'ul.anh-jobs',
 			percentPosition: true,
 			gutter: 0
@@ -246,6 +246,89 @@ $('ul.list-menu li').click(function(event) {
 
 return false;
 });
+
+$(window).resize(function(event) {
+
+
+var chieurong=$(window).width();
+
+
+
+if(chieurong<930){
+	$('ul.list-menu li:nth-child(1)').click(function(event) {
+		$('body,html').animate({scrollTop:$('.about').offset().top},1500,"easeInOutExpo")
+	});
+	$('ul.list-menu li:nth-child(2)').click(function(event) {
+		$('body,html').animate({scrollTop:$('.resume').offset().top},1500,"easeInOutExpo")
+	});
+	$('ul.list-menu li:nth-child(3)').click(function(event) {
+		$('body,html').animate({scrollTop:$('.work').offset().top},1500,"easeInOutExpo")
+	});
+	$('ul.list-menu li:nth-child(4)').click(function(event) {
+		$('body,html').animate({scrollTop:$('.contacts').offset().top},1500,"easeInOutExpo")
+	});
+
+
+	 $(window).scroll(function(event) {
+	var cuonweb=$('body,html').scrollTop();
+if(cuonweb>100){
+	$('ul.list-menu').addClass('biendoi');
+}
+else if(cuonweb<100){
+	$('ul.list-menu').removeClass('biendoi');
+}
+
+
+});
+}
+});
+
+
+	 $(window).scroll(function(event) {
+	var cuonweb=$('body,html').scrollTop();
+if(cuonweb>100){
+	$('ul.list-menu').addClass('biendoi');
+}
+else if(cuonweb<100){
+	$('ul.list-menu').removeClass('biendoi');
+}
+
+
+});
+
+var chieurong=$(window).width();
+
+if(chieurong<930){
+	$('ul.list-menu li:nth-child(1)').click(function(event) {
+		$('body,html').animate({scrollTop:($('.about').offset().top)-100 },1000,"easeInOutExpo")
+	});
+	$('ul.list-menu li:nth-child(2)').click(function(event) {
+		$('body,html').animate({scrollTop:($('.resume').offset().top)-100 },1000,"easeInOutExpo")
+	});
+	$('ul.list-menu li:nth-child(3)').click(function(event) {
+		$('body,html').animate({scrollTop:($('.work').offset().top)-100 },1000,"easeInOutExpo")
+	});
+	$('ul.list-menu li:nth-child(4)').click(function(event) {
+		$('body,html').animate({scrollTop:($('.contacts').offset().top)-100},1000,"easeInOutExpo")
+	});
+
+
+
+	 $(window).scroll(function(event) {
+	var cuonweb=$('body,html').scrollTop();
+if(cuonweb>300){
+	$('ul.list-menu').addClass('biendoi');
+}
+else if(cuonweb<300){
+	$('ul.list-menu').removeClass('biendoi');
+}
+
+
+});
+}
+
+
+
 
 
 
